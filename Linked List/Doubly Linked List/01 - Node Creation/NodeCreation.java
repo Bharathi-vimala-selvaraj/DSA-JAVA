320766b75f0d04ae573dc1991dc7d4d7f48c2070
Bharathi-vimala-selvaraj <bharathivimalaselvaraj@gmail.com>
2026-08-04 15:47:48 +0530

    Create  NodeCreation.java

---
+++ Linked List/Doubly Linked List/01 - Node Creation/NodeCreation.java
@@ -0,0 +1,18 @@
+class Node {
+
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class DoublyLinkedList {
+
+    Node head;
+
+}
